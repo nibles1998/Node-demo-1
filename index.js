5b2a3d606a665c2760bd7b4f402ba4e2e2bace6e
nibles1998 <longvynguyenhoang@gmail.com>
2020-02-10 15:36:17 +0700

    Modify Server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { PORT } = require('./config');
+const initRoot = require('./routes');
 
 const app = express();
 
-app.listen();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+initRoot(app);
+
+app.listen(PORT,() => {
+    console.log(`Server is running with port ${PORT}`);
+});
